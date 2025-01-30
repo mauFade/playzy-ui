6c3254ace58504f95fd41c75d6449ce73423a5b4
Mauricio Cardoso <mauricio.cds00@gmail.com>
2025-01-29 21:39:27 -0300

    feat: improve header for sticky

--- src/app/sessions/page.tsx
+++ src/app/sessions/page.tsx
@@ -27,38 +27,38 @@ const Sessions = () => {
   });
 
   return (
-    <div className="bg-gradient-to-br from-zinc-900 to-zinc-950 min-h-screen flex flex-col">
+    <>
       <Header />
+      <div className="bg-gradient-to-br from-zinc-900 to-zinc-950 min-h-screen flex flex-col">
+        <main className="flex-1 flex flex-col items-center py-10 md:mt-12">
+          <div className="container mx-auto px-6">
+            <h1 className="text-3xl font-extrabold text-teal-500 mb-6 text-center">
+              Sessões Abertas
+            </h1>
 
-      <main className="flex-1 flex flex-col items-center py-10">
-        <div className="container mx-auto px-6">
-          <h1 className="text-3xl font-extrabold text-teal-500 mb-6 text-center">
-            Sessões Abertas
-          </h1>
+            {isPending ? (
+              <div className="flex justify-center items-center">
+                <AiOutlineLoading className="text-4xl text-teal-500 animate-spin" />
+              </div>
+            ) : (
+              <div className=" gap-6">
+                {data &&
+                  data.sessions.map((game: any) => (
+                    <SessionItem key={game.id} {...game} />
+                  ))}
+              </div>
+            )}
 
-          {isPending ? (
-            <div className="flex justify-center items-center">
-              <AiOutlineLoading className="text-4xl text-teal-500 animate-spin" />
-            </div>
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-              {data &&
-                data.sessions.map((game: any) => (
-                  <SessionItem key={game.id} {...game} />
-                ))}
-            </div>
-          )}
-
-          <Carousel
-            page={page}
-            setPage={setPage}
-            totalPages={data ? data.total_pages : 100}
-          />
-        </div>
-      </main>
-
-      <Footer />
-    </div>
+            <Carousel
+              page={page}
+              setPage={setPage}
+              totalPages={data ? data.total_pages : 100}
+            />
+          </div>
+        </main>
+        <Footer />
+      </div>{" "}
+    </>
   );
 };
 
