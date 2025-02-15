ceeaafd7c5838fb72de184e6c1ff2ee34dd97a0d
Mauricio Cardoso <mauricio.cds00@gmail.com>
2025-02-15 11:52:18 -0300

    feat: add popover for sessions

--- src/app/(private)/sessions/page.tsx
+++ src/app/(private)/sessions/page.tsx
@@ -5,16 +5,22 @@ import { Gamepad2, LoaderCircle } from "lucide-react";
 import React, { useState } from "react";
 
 import { api } from "@/api/api";
+import { SessionInterface } from "@/api/dto/sessions";
 import Carousel from "@/components/carousel";
 import { AvatarImage, Avatar, AvatarFallback } from "@/components/ui/avatar";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 import CreateSessionModal from "./components/create-session-modal";
 import Drawerform from "./components/drawer-form";
@@ -26,6 +32,9 @@ const fetchSessions = async (page: number) => {
 
 const Sessions = () => {
   const [page, setPage] = useState<number>(1);
+  const [selectedSession, setSelectedSession] =
+    useState<SessionInterface | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   const { data } = useQuery({
     queryKey: ["sessions", page],
@@ -38,7 +47,14 @@ const Sessions = () => {
       <section className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         {data ? (
           data.sessions.map((session, i) => (
-            <Card key={i}>
+            <Card
+              key={session.id}
+              className="hover:cursor-pointer"
+              onClick={() => {
+                setSelectedSession(session);
+                setIsModalOpen(true);
+              }}
+            >
               <CardHeader>
                 <div className="flex items-center justify-center">
                   <CardTitle className="text-lg sm:text-xl select-none">
@@ -99,16 +115,68 @@ const Sessions = () => {
                     .join("-")}
                 </p>
               </CardContent>
-
-              <CardFooter>
-                <Drawerform />
-              </CardFooter>
             </Card>
           ))
         ) : (
           <LoaderCircle className="animate-spin" />
         )}
       </section>
+
+      <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <DialogContent className="max-w-80 sm:max-w-3xl">
+          <DialogHeader>
+            <DialogTitle className="text-2xl">
+              {selectedSession?.game}
+            </DialogTitle>
+          </DialogHeader>
+
+          {selectedSession && (
+            <div className="space-y-4">
+              <div className="flex items-center space-x-2">
+                <Avatar className="w-8 h-8">
+                  <AvatarImage src="https://www.github.com/mauFade.png" />
+                  <AvatarFallback>MC</AvatarFallback>
+                </Avatar>
+                <span className="font-semibold">
+                  {selectedSession.user.gamertag}
+                </span>
+              </div>
+
+              <div className="grid gap-2">
+                <p className="text-lg">
+                  Modo de jogo:{" "}
+                  <span className="font-semibold">
+                    {selectedSession.is_ranked ? "Ranked" : "Casual"}
+                  </span>
+                </p>
+                {selectedSession.rank && (
+                  <p className="text-lg">
+                    Ranque:{" "}
+                    <span className="font-semibold">
+                      {selectedSession.rank}
+                    </span>
+                  </p>
+                )}
+                <p className="text-lg">
+                  Objetivo:{" "}
+                  <span className="font-semibold">
+                    {selectedSession.objetive}
+                  </span>
+                </p>
+                <p className="text-sm text-muted-foreground">
+                  Criado em:{" "}
+                  {new Date(selectedSession.created_at)
+                    .toLocaleDateString("pt-BR")
+                    .split("/")
+                    .join("-")}
+                </p>
+              </div>
+
+              <Drawerform />
+            </div>
+          )}
+        </DialogContent>
+      </Dialog>
 
       <Carousel
         page={page}
